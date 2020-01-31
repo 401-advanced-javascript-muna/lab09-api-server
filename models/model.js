b0052d91a15499a3fb4861946e4e1ec499c30f98
muna alshorman <munaalshorman123@gmail.com>
2020-01-31 23:46:27 +0200

    lab9

--- models/model.js
+++ models/model.js
@@ -17,13 +17,13 @@ class OurModel{
     let newRecord = new this.schema(record);
     return newRecord.save();
   }
-//     update(_id, record) {
-//       return schema.findByIdAndUpdate(_id, record, { new: true });
-//     }
+    update(_id, record) {
+      return schema.findByIdAndUpdate(_id, record, { new: true });
+    }
 
-//   delete(_id) {
-//     return schema.findByIdAndDelete(_id);
-//   }
+  delete(_id) {
+    return schema.findByIdAndDelete(_id);
+  }
 }
 
 
